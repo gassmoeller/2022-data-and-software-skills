8946b00cb3140be79e7402a7123791e79fa6e511
Ashley Dann <dannashley03@gmail.com>
2022-04-08 20:20:43 -0400

    tests

---
+++ tests/test_plotting.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+"""Main tests our plotting script"""
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sys.path.append(os.path.join(
+    os.path.dirname(__file__),
+    ".."))
+
+from src import data_script
+
+def test_plot(): 
+    assert 1 == 0
+    plotting
