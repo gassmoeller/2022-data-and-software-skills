1dc98605623f838f61fd6ea5a01ebad29b3012e8
Rene Gassmoeller <rene.gassmoeller@mailbox.org>
2022-04-20 12:17:30 -0400

    Review of the dann_a7 branch

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -5,8 +5,6 @@
 import sys
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
 sys.path.append(os.path.join(
     os.path.dirname(__file__),
@@ -15,6 +13,7 @@ sys.path.append(os.path.join(
 from src import data_script
 
 def test_csv_to_json():
+    """Tests the csv_to_json function"""
     current_file_location = os.path.dirname(__file__)
 
     data_directory = os.path.join(current_file_location,
@@ -30,11 +29,13 @@ def test_csv_to_json():
     data_script.csv_to_json(conversion_filename, json_filename)
 
 def test_process_data():
+    """Tests the process_data function"""
     test_input_data = np.array([[0,32],[1,212]])
     test_output = data_script.process_data(test_input_data)
     test_expected_output = np.array([[0,32,273],[1,212,373]])
 
 def test_plot_data():
+    """Tests the plot_data function"""
     current_file_location = os.path.dirname(__file__)
     results_directory = os.path.join(current_file_location,
                                         "..",
